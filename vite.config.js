d0b1b1fec186aaaa6f43641c8af76940e1d91707
lautaroPH <lautaroph@gmail.com>
2022-06-05 11:49:53 -0300

    add baseUrl

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import path from 'path';
+import { readdirSync } from 'fs';
 
-// https://vitejs.dev/config/
+const absolutePathAliases = {};
+// Root resources folder
+const srcPath = path.resolve('./src/');
+// Ajust the regex here to include .vue, .js, .jsx, etc.. files from the resources/ folder
+const srcRootContent = readdirSync(srcPath, { withFileTypes: true }).map(
+  (dirent) => dirent.name.replace(/(\.jsx){1}(x?)/, '')
+);
+
+srcRootContent.forEach((directory) => {
+  absolutePathAliases[directory] = path.join(srcPath, directory);
+});
+
 export default defineConfig({
   plugins: [react()],
+
+  root: './',
+  resolve: {
+    alias: {
+      ...absolutePathAliases,
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
 });
