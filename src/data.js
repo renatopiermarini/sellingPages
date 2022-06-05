0016f3bfe67353f8a57ea9081920cb4f002862c6
lautaroPH <lautaroph@gmail.com>
2022-06-05 19:10:52 -0300

    add data file

---
+++ src/data.js
@@ -0,0 +1,33 @@
+import number1 from 'assets/lottie/number1.json';
+import number2 from 'assets/lottie/number2.json';
+import number3 from 'assets/lottie/number3.json';
+import number4 from 'assets/lottie/number4.json';
+
+const howWeWork = [
+  {
+    title: 'Discovery',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure minus similique officia corporis quos possimus facere optio minima pariatur mollitia consequatur placeat, velit repellat reiciendis dicta? Qui, accusantium. Unde, rem.',
+    number: number1,
+  },
+  {
+    title: 'Planning',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure minus similique officia corporis quos possimus facere optio minima pariatur mollitia consequatur placeat, velit repellat reiciendis dicta? Qui, accusantium. Unde, rem.',
+    number: number2,
+  },
+  {
+    title: 'Build',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure minus similique officia corporis quos possimus facere optio minima pariatur mollitia consequatur placeat, velit repellat reiciendis dicta? Qui, accusantium. Unde, rem.',
+    number: number3,
+  },
+  {
+    title: 'Delivery',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure minus similique officia corporis quos possimus facere optio minima pariatur mollitia consequatur placeat, velit repellat reiciendis dicta? Qui, accusantium. Unde, rem.',
+    number: number4,
+  },
+];
+
+export { howWeWork };
